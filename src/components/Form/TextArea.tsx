f9b04f55e4c23b7953438658e66c1012c538a4e2
Paul Miranda Vega <paul.miranda@sparktechs.com>
2021-09-27 12:26:23 -0700

    created text area compontent

---
+++ src/components/Form/TextArea.tsx
@@ -0,0 +1,9 @@
+import React, { ForwardRefRenderFunction, InputHTMLAttributes } from 'react'
+
+const TextArea: React.FC<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>> = ({ className, ...rest }) => {
+    return (
+        <textarea className={`form__textarea ${className}`} {...rest} />
+    )
+}
+
+export default TextArea;
--- src/layout/AddPhoto.tsx
+++ src/layout/AddPhoto.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import Button from '../components/Button/Button';
+import TextArea from '../components/Form/TextArea';
+import IconImageOutline from '../svg/IconImageOutline';
 
 const AddPhoto = () => {
     const { t } = useTranslation();
     const [file, setFile] = useState<string | undefined>(undefined);
+    const [description, setDescription] = useState<string>("");
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
@@ -14,15 +17,23 @@ const AddPhoto = () => {
             }
         }
     }
+
+    const isButtonEnabled = () => {
+        if (file && description !== "") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     return (
         <main className="add-photo">
             <div className="add-photo__container">
-                {file && <img src={file} alt="Selected item" />}
+                {file ? <img src={file} alt="Selected item" className="add-photo__img" /> : <IconImageOutline className="add-photo__img" />}
                 <label htmlFor="addFileBtn" className="add-photo__custom">{t("add_photo.add_button")}</label>
                 <input onChange={handleChange} id="addFileBtn" type="file" className="add-photo__btn" accept=".jpg, .jpeg, .png" />
-                {
-                    file && <Button fullwidth style={{ marginTop: '2rem' }}>{t("add_photo.upload_button")}</Button>
-                }
+                <TextArea value={description} onChange={e => setDescription(e.target.value)} className="add-photo__description" name="description" id="description" cols={30} rows={10} />
+                <Button disabled={isButtonEnabled()} className="add-photo__upload" fullwidth style={{ marginTop: '2rem' }}>{t("add_photo.upload_button")}</Button>
             </div>
         </main>
     )
