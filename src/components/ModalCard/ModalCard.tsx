9bb746c880b300ef2faee929060c28b8f469c944
Paul Miranda Vega <paul.miranda@sparktechs.com>
2021-09-28 17:39:20 -0700

    added ModalCard component

---
+++ src/components/ModalCard/ModalCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import IconChatBubbleElipsesOutline from '../../svg/IconChatBubbleElipsesOutline';
+import IconHeartOutline from '../../svg/IconHeartOutline';
+import Heading from '../Heading/Heading';
+import Post from '../../types/Post';
+
+interface IProps {
+    post: Post
+}
+const ModalCard: React.FC<IProps> = ({ post }) => {
+    return (
+        <div className="modal-card">
+            <img className="modal-card__img" src={post.img} alt={post.public_id} />
+            <div className="modal-card__content">
+                <Heading centered type="subtitle">{post.user.username}</Heading>
+                <div className="modal-card__icons">
+                    <div className="modal-card__icon-box">
+                        <IconHeartOutline />
+                        {post.likeCount}
+                    </div>
+                    <div className="modal-card__icon-box">
+                        <IconChatBubbleElipsesOutline />
+                        {post.comments.length}
+                    </div>
+                </div>
+                <p className="modal-card__description">
+                    {post.description}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ModalCard
--- src/layout/Profile.tsx
+++ src/layout/Profile.tsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router';
 import api from '../api/api';
 import Heading from '../components/Heading/Heading';
 import Modal from '../components/Modal/Modal';
-import Post from '../interfaces/Post';
+import ModalCard from '../components/ModalCard/ModalCard';
+import Post from '../types/Post';
 import IconChatBubbleElipsesOutline from '../svg/IconChatBubbleElipsesOutline';
 import IconHeartOutline from '../svg/IconHeartOutline';
 
@@ -54,44 +55,9 @@ const Profile: React.FC<IProps> = () => {
                 }
             </div>
             <Modal visible={open} onClose={onModalClose}>
-                <div className="profile__modal">
-                    <img className="profile__modal__img" src={postSelected?.img} alt={postSelected?.public_id} />
-                    <div className="profile__modal__content">
-                        <Heading centered type="subtitle">{postSelected?.user.username}</Heading>
-                        <div className="profile__modal__icons">
-                            <div className="profile__modal__icon-box">
-                                <IconHeartOutline />
-                                {postSelected?.likeCount}
-                            </div>
-                            <div className="profile__modal__icon-box">
-                                <IconChatBubbleElipsesOutline />
-                                9
-                            </div>
-                        </div>
-                        <p className="profile__modal__description">
-                            {postSelected?.description}
-                        </p>
-                    </div>
-                    {/* <figure className="profile__modal__frame">
-                        <img className="profile__modal__img" src={postSelected?.img} alt={postSelected?.public_id} />
-                    </figure>
-                    <div className="profile__modal__content">
-                        <Heading centered type="subtitle">{postSelected?.user.username}</Heading>
-                        <div className="profile__modal__icons">
-                            <div className="profile__modal__icon-box">
-                                <IconHeartOutline />
-                                {postSelected?.likeCount}
-                            </div>
-                            <div className="profile__modal__icon-box">
-                                <IconChatBubbleElipsesOutline />
-                                9
-                            </div>
-                        </div>
-                        <p className="profile__modal__description">
-                            {postSelected?.description}
-                        </p>
-                    </div> */}
-                </div>
+                {
+                    postSelected && <ModalCard post={postSelected} />
+                }
             </Modal>
         </div>
     )
